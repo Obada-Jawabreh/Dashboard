7c10d51cadc37ec0b2f999377172d6d72f897b33
Obada-Jawabreh <obadajawabreh6@gmail.com>
2024-08-20 11:58:26 +0300

    done

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -1,15 +1,15 @@
 import "./app.css";
 
 import Dashboard from "./Components/Dashboard";
-import PartyVoteTable from "./Components/PartyTable";
-import VoteTable from "./Components/VoteTable";
+
+
 
 function App() {
   return (
     <>
-      {/* <Dashboard/> */}
-      <VoteTable />
-      <PartyVoteTable />
+
+      <Dashboard/>
+    
     </>
   );
 }
